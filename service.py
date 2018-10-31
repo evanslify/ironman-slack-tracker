881caaea460f1d4e3003a5938276054b513e2198
es <es@evsfy.com>
2018-10-31 18:39:01 +0800

    Update requirements.txt

--- service.py
+++ service.py
@@ -87,7 +87,7 @@ def post_slack():
         'icon_emoji': ':truck:',
         'text': get_all_not_completed()
     }
-    requests.post('slack-webhook-url', json=payload)
+    requests.post('slack webhook url', json=payload)
 
 
 def handler(event, context):
